e7c37a55c35194ef475ba1e63f7e9077d287a1cf
Flavio Melo <flaviomelofilho.15@gmail.com>
2025-09-22 11:28:50 -0300

    projeto Angular 16 com SCSS - CSS do Material Table

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 import { MatTableModule } from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from "@angular/material/toolbar";
+
 
 @NgModule({
   declarations: [CoursesComponent],
-  imports: [CommonModule, CoursesRoutingModule, MatTableModule],
+  imports: [CommonModule, CoursesRoutingModule, MatTableModule, MatCardModule, MatToolbarModule],
 })
 export class CoursesModule {}
