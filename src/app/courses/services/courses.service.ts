d6cac7d352476288c3428874ffbf15697f4c1a94
Flavio Melo <flaviomelofilho.15@gmail.com>
2025-09-30 10:31:12 -0300

    projeto Angular 16 com SCSS - Add de package.json para comunicação com o back-end

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -7,14 +7,14 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class CoursesService {
-  private readonly API = '/assets/courses.json';
+  private readonly API = 'api/courses';
 
   constructor(private httpClient: HttpClient) {}
 
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
-      delay(2000),
+      //delay(2000),
       tap((courses) => console.log(courses))
     );
   }
