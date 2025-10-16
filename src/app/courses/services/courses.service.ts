a1761833db9f9f8984abb0bcb4d83730977501a0
Flavio Melo <flaviomelofilho.15@gmail.com>
2025-10-16 09:00:41 -0300

    projeto Angular 16 com SCSS - Atualizar Curso: Salvando o Formulário

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -19,11 +19,24 @@ export class CoursesService {
     );
   }
 
-  loadById(id:string){
+  loadById(id: string) {
     return this.httpClient.get<Course>(`${this.API}/${id}`);
   }
 
-  save(course: Partial <Course>) {
-    return this.httpClient.post<Course>(this.API, course);
+  save(record: Partial<Course>) {
+    if (record._id) {
+      return this.updade(record);
+    }
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private updade(record: Partial<Course>) {
+    return this.httpClient
+      .put<Course>(`${this.API}/${record._id}`, record)
+      .pipe(first());
   }
 }
