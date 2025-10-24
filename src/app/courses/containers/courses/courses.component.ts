28d75d8b770a6498e505b3c8c0184063d920c5ca
Flavio Melo <flaviomelofilho.15@gmail.com>
2025-10-24 10:14:26 -0300

    projeto Angular 16 com SCSS - Dialog de Confirmação

--- src/app/courses/containers/courses/courses.component.ts
+++ src/app/courses/containers/courses/courses.component.ts
@@ -5,6 +5,9 @@ import { Course } from '../../model/course';
 import { catchError, Observable, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Location } from '@angular/common';
+import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -12,7 +15,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent {
-  courses$: Observable<Course[]>;
+  courses$: Observable<Course[]> | null = null;
 
   // coursesService:CoursesService;
 
@@ -20,8 +23,14 @@ export class CoursesComponent {
     private coursesService: CoursesService,
     public dialog: MatDialog,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private snackBar: MatSnackBar,
+    private location: Location
   ) {
+    this.refresh();
+  }
+
+  refresh() {
     this.courses$ = this.coursesService.list().pipe(
       catchError((Error) => {
         this.onError('Erro ao carregar cursos.');
@@ -40,7 +49,38 @@ export class CoursesComponent {
     this.router.navigate(['new'], { relativeTo: this.route });
   }
 
-  onEdit(course: Course){
-    this.router.navigate(['edit',course._id],{relativeTo: this.route})
+  onEdit(course: Course) {
+    this.router.navigate(['edit', course._id], { relativeTo: this.route });
+  }
+
+  onRemove(course: Course) {
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: 'Tem certeza que deseja remover o Curso?',
+    });
+
+    dialogRef.afterClosed().subscribe((result: Boolean) => {
+      if (result) {
+        this.coursesService.remove(course._id).subscribe({
+          next: (result) => {
+            this.refresh();
+            console.log('Deletado:', result);
+            this.snackBar.open('Curso deletado com sucesso!', 'Fechar', {
+              duration: 3000,
+            });
+            this.onCancel();
+          },
+          error: (error) => {
+            console.error('Erro:', error);
+            this.snackBar.open('Erro ao deletar o curso.', 'Fechar', {
+              duration: 3000,
+            });
+          },
+        });
+      }
+    });
+  }
+
+  onCancel() {
+    this.location.back();
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { CategoryPipe } from './pipes/category.pipe';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
-  declarations: [ErrorDialogComponent, CategoryPipe],
-  imports: [CommonModule, MatDialogModule],
-  exports: [ErrorDialogComponent,CategoryPipe],
+  declarations: [ErrorDialogComponent, CategoryPipe, ConfirmationDialogComponent],
+  imports: [CommonModule, MatDialogModule,MatButtonModule],
+  exports: [ErrorDialogComponent,CategoryPipe,ConfirmationDialogComponent],
 })
 export class SharedModule {}
